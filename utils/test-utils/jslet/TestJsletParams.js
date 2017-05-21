1a51a9fc0d96e2617e9e93825408a2253b3ac9ed
Pascal ECHEMANN <developers@flashapi.org>
2017-05-21 21:37:48 +0200

    Minor Update
    Removing .js files from the "utils" package.

--- utils/test-utils/jslet/TestJsletParams.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PARAMS = {
-    name: "Status",
-    urlPatterns: ["/status"],
-    template: "/views/status.ejs"
-};
